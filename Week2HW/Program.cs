a7075b6dbaab45ae42486db9539ec2fb17112fc0
Phillip Jackson <pj37883@email.vccs.edu>
2022-01-14 17:20:20 -0500

    Add project files.

---
+++ Week2HW/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Week2HW
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double oil, tire, inspection;
+            double total;
+            string inputString;
+
+            WriteLine("***Welcome to Phil's One-Stop Auto Shop***");
+            ReadKey();
+
+            WriteLine("How much was your oil change?");
+            inputString = ReadLine();
+            oil = Convert.ToDouble(inputString);
+
+            WriteLine("How much was your tire change?");
+            tire = Convert.ToDouble(ReadLine());
+
+            WriteLine("How much was your car inspection?");
+            inspection = Convert.ToDouble(ReadLine());
+
+            total = oil + tire + inspection;
+            
+            Write(total);
+            
+            ReadKey();
+
+
+            total = oil + tire + inspection
+
+
+
+
+        }
+    }
+}
